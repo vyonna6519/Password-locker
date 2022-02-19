032bdc1892eecc5f587762fecd3857c986602007
vyonna <vyonna.njenga@student.moringaschool.com>
2022-02-19 17:16:47 +0300

    created class account

--- account.py
+++ account.py
@@ -2,7 +2,7 @@ class Account:
     """
     Class that generates new instances of users.
     """
-    account_list = [] #empty account list
+    account_list = [] 
 
     def __init__(self,account_name,user_name,password,email):
         self.account_name = account_name
